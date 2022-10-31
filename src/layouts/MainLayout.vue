79584adff0bc0e7d147dfb5d3f168b913128bcd2
Lucas carvalho <lucas.amorin@voxdatati.com.br>
2022-10-31 14:19:22 -0300

    mudança layout da pagina

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,6 +1,69 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated> </q-header>
+    <q-header elevated>
+      <q-toolbar class="barra">
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="../../public/icons/Helix.png" />
+          </q-avatar>
+          Estudos
+        </q-toolbar-title>
+        <div>...</div>
+      </q-toolbar>
+    </q-header>
+    <q-drawer
+      style="background-color: #121929"
+      show-if-above
+      v-model="leftDrawerOpen"
+      side="left"
+    >
+      <!-- drawer content -->
+      <div class="text-h6 text-center">MENU</div>
+      <div class="q-pt-m column">
+        <q-expansion-item
+          dense
+          class="text-center teste"
+          label="Front-End Projects"
+          icon="fab fa-dev"
+        >
+          <div class="column">
+            <q-btn
+              flat
+              dense
+              style="color: black"
+              label="Equilibrium"
+              icon="fas fa-cube"
+              @click="goEquilibrium()"
+            />
+            <q-btn
+              flat
+              dense
+              style="color: black"
+              label="ydStudio"
+              icon="fas fa-cube"
+              @click="goysStudio()"
+            />
+          </div>
+        </q-expansion-item>
+        <q-expansion-item
+          dense
+          class="text-center teste"
+          label="Hardware Projects"
+          icon="fas fa-microchip"
+        >
+        </q-expansion-item>
+        <q-btn
+          flat
+          dense
+          class="text-left"
+          color="primary"
+          icon="home"
+          label="Home"
+          :to="'/'"
+        />
+      </div>
+    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -9,13 +72,40 @@
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 
-export default defineComponent({
-  name: "MainLayout",
-
+export default {
   setup() {
-    return {};
+    const leftDrawerOpen = ref(false);
+    const router = useRouter();
+    function toggleLeftDrawer() {
+      leftDrawerOpen.value = !leftDrawerOpen.value;
+    }
+    function goEquilibrium() {
+      router.push({ name: "equilibrium" });
+    }
+    function goysStudio() {
+      router.push({ name: "ydstudio" });
+    }
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer,
+      goEquilibrium,
+      goysStudio,
+    };
   },
-});
+};
 </script>
+
+<style scoped>
+.barra {
+  background-color: #121929;
+}
+.lateral {
+  background-color: #056367;
+}
+.teste >>> .q-item__section--main ~ .q-item__section--side {
+  padding: 0px !important;
+}
+</style>
---
+++ src/pages/ydStudio.vue
@@ -0,0 +1,101 @@
+<template>
+  <q-page class="flex flex-center" style="background-color: #59493a">
+    <!-- {{ teste }} -->
+    <q-card
+      class="my-card text-white"
+      style="
+        width: 290px;
+        height: 480px;
+        border-radius: 10px;
+        background-color: #432d1a;
+      "
+    >
+      <q-card-section class="text-center q-pb-s">
+        <img
+          class="isso"
+          src="../../public/lavabo.png"
+          alt="Equilibrium logo"
+          style="width: 250px; height: 250px; border-radius: 10px"
+        />
+        <div class="ocara"></div>
+      </q-card-section>
+      <q-card-section class="text-white q-pt-none q-pb-none">
+        <div class="text-h6 cursor-pointer xablau">Lavabo Representação 3D</div>
+        <div class="text-subtitle2 text-blue-grey">
+          Esse Render foi um dos meus primeiros contatos com vray. Na época...
+        </div>
+      </q-card-section>
+      <q-card-section class="row">
+        <div class="text-red-13 text-weight-bold">
+          <q-icon name="favorite" size="15px" />
+          <span class="col text-left">4k</span>
+        </div>
+        <div class="col text-right text-blue-grey">
+          <q-icon name="mode_comment" />
+          60 comentários
+        </div>
+      </q-card-section>
+      <q-separator inset style="background-color: white" />
+      <q-card-section class="q-pa-none">
+        <q-item class="items-center">
+          <q-item-section class="q-pa-none col-auto" top>
+            <q-avatar class="overlapping">
+              <q-img
+                src="../../public/po.png"
+                style="
+                  width: 35px;
+                  height: 35px;
+                  border: 1px solid white;
+                  border-radius: 20px;
+                "
+              />
+            </q-avatar>
+          </q-item-section>
+          <span class="text-blue-grey text-weight-medium q-pr-sm"
+            >Criado por </span
+          ><span class="cursor-pointer xablau"> Ylanna Dâmaris</span>
+        </q-item>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import axios from "axios";
+
+export default defineComponent({
+  name: "IndexPage",
+  setup() {
+    // getTeste();
+    // function getTeste() {
+    //   axios
+    //     .get("http://api.open-notify.org/iss-now.json")
+    //     .then((res) => {
+    //       console.log(res);
+    //     })
+    //     .catch((err) => alert(err));
+    // }
+    return {};
+  },
+});
+</script>
+<style scoped>
+.ocara {
+  position: absolute;
+  top: 17px;
+  left: 20px;
+  width: 250px;
+  height: 250px;
+  z-index: 10000;
+}
+.ocara:hover {
+  cursor: pointer;
+  background-color: hsl(178, 100%, 50%);
+  opacity: 0.2;
+  border-radius: 10px;
+}
+.xablau:hover {
+  color: hsl(178, 100%, 50%);
+}
+</style>
